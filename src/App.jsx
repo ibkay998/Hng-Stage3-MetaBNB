86df98be886127f135c71bfbe89c99b3f4836a5c
ibukunolouwa oyeniyi <ibukun46@gmail.com>
2022-11-11 09:41:35 +0100

    pkg:installed tailwind charaui and vite

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+
+function App() {
+  return <div className='bg-red-400  h-10'>Testing jj this is true</div>;
+}
+
+export default App;
