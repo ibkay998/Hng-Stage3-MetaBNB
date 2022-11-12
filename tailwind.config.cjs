ff5ae1a72aa3bd8521eb85aac0cd5bc86d971d97
ibukunolouwa oyeniyi <ibukun46@gmail.com>
2022-11-12 16:51:00 +0100

    pkg:modified tailwind pkg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'rose': ['"Red Rose"', 'cursive'],
+        'sora': ['"Sora"', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 };
