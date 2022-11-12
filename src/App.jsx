0c2af3269a274878091112035be9a810de8bbc5a
ibukunolouwa oyeniyi <ibukun46@gmail.com>
2022-11-12 16:54:41 +0100

    refactor:added layout for generic page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
+import { Routes, Route, Link } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Home from './pages/Home/Home';
+import PlaceToStay from './pages/PlaceToStay/PlaceToStay';
 
 function App() {
-  return <div className='bg-red-400  h-10'>Testing jj this is true</div>;
+  return (
+    <div className='w-full'>
+      <Layout>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='place-to-stay' element={<PlaceToStay />} />
+        </Routes>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
