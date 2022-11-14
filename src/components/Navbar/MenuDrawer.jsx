a6cc995a627362fd5a78b543c468451dffe79b2f
ibukunolouwa oyeniyi <ibukun46@gmail.com>
2022-11-14 09:45:46 +0100

    style:fixed responsive nav bar

--- src/components/Navbar/MenuDrawer.jsx
+++ src/components/Navbar/MenuDrawer.jsx
@@ -1,31 +1,41 @@
-import React from 'react'
+import React from 'react';
 import {
-    Drawer,
-    DrawerBody,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerCloseButton,
-  } from '@chakra-ui/react'
+  Drawer,
+  DrawerBody,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from '@chakra-ui/react';
 
-function MenuDrawer({isOpen,onClose,btnRef,lists}) {
+function MenuDrawer({ isOpen, onClose, btnRef, lists }) {
   return (
     <Drawer
-        isOpen={isOpen}
-        placement='right'
-        onClose={onClose}
-        finalFocusRef={btnRef}
-      >
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerBody>
-            {lists.map((list)=>(
-                <a href={list.link} className="" >{list.name}</a>
+      isOpen={isOpen}
+      placement='right'
+      onClose={onClose}
+      finalFocusRef={btnRef}
+    >
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton color='white' />
+        <DrawerBody
+          bg='#A02279'
+          
+        >
+          <div className='flex flex-col font-rose font-bold text-lg w-full first:mt-8'>
+            {lists.map((list) => (
+              <a
+                href={list.link}
+                className='text-white flex p-4 w-full items-center justify-center hover:bg-white hover:text-[#A02279]'
+              >
+                {list.name}
+              </a>
             ))}
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
-  )
+          </div>
+        </DrawerBody>
+      </DrawerContent>
+    </Drawer>
+  );
 }
 
-export default MenuDrawer
+export default MenuDrawer;
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { ChakraProvider } from '@chakra-ui/react';
+
+import { ChakraProvider,extendTheme } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
+
+const theme = extendTheme({
+  components:{
+    Drawer:{
+      parts: ['dialog', 'header', 'body'],
+      variants: {
+        primary: {
+          dialog: {
+            background: 'red', 
+          }
+        },
+        secondary: {
+          dialog: {
+            background: 'green'
+          }
+        }
+      },
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
