86f5219f9af1455ec812558597c3e932008edd0f
ibukunolouwa oyeniyi <ibukun46@gmail.com>
2022-11-14 12:44:48 +0100

    style:fixed padding in modal

--- src/components/Navbar/WalletButton.jsx
+++ src/components/Navbar/WalletButton.jsx
@@ -6,7 +6,7 @@ function WalletButton({name,icon}) {
   return (
     <div className='hover:bg-[#F8F9FA] w-full flex-col  flex cursor-pointer mt-[16px] border-2 border-[#CFD8DC] rounded-md'>
         <div className='flex justify-between items-center'>
-            <div className='flex items-center'>
+            <div className='flex items-center p-2'>
                 <img src={icon} alt="" />
                 <p className='font-sora ml-[16px]'>{name}</p>
             </div>
